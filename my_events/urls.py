4f6253a05ebd903a330d58867bd3026598c34024
sadiqsuraj <surajsadiqsalihu@gmail.com>
2025-11-05 08:34:55 +0100

    fix: make routes lowercase for production

--- my_events/urls.py
+++ my_events/urls.py
@@ -21,7 +21,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path("api/Events/", include('Events.urls')),
+    path("api/events/", include('Events.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
